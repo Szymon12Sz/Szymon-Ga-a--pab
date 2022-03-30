4b24de77e5c6bdf315c0bd174a7e31d8aafd72d5
Szymon Gałaś <szymon.galas@microsoft.wsei.edu.pl>
2022-03-30 18:53:14 +0200

    commit

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
-app.get('/', function (req, res) {
+var num1 = req.query;
+var num2 = req.query;
+app.get('/', function (req, res) 
+{
     res.send('Hello World');
+     
 });
 app.listen(3000);
--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 const express = require('express')  
 const app = express()  
-app.get('/', function (req, res) {  
-  res.send('Hello World')  
+app.get('/', function (req, res) 
+{  
+  res.send('Hello World Hello')  
+  res.send('2+3'); 
 })  
 app.listen(3000)  
